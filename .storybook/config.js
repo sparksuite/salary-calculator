3961b0f22b7da660ae320ae93364435dfea092a6
Wes Cossick <wes@sparksuite.com>
2019-09-23 14:08:13 -0500

    Format code

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,4 @@ import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
 
 // automatically import all files ending in *.stories.js
-configure(
-	requireContext('../src/components', true, /\.stories\.tsx$/),
-	module
-);
+configure(requireContext('../src/components', true, /\.stories\.tsx$/), module);
