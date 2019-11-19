7db43cf4d6a465c8ef704629ebf5dbd0a3679cb0
Wes Cossick <wes@sparksuite.com>
2019-11-19 14:50:10 -0600

    Update descriptors

--- src/components/compensation.test.tsx
+++ src/components/compensation.test.tsx
@@ -6,7 +6,7 @@ import store from '../redux/store';
 import { Provider } from 'react-redux';
 
 // Tests
-it('renders shallowly', () => {
+it('renders', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
 		<Provider store={store}>
