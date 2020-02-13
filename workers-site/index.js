99b84d398d2d1709b61b14472f746e4bd4ad3b25
Wes Cossick <WesCossick@users.noreply.github.com>
2020-02-13 09:43:37 -0600

    Update Workers Sites' index file
    This follows patterns set by newer apps we've been working on

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,80 +1,49 @@
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
-
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
-const DEBUG = false
+import { getAssetFromKV, serveSinglePageApp } from '@cloudflare/kv-asset-handler'
 
 addEventListener('fetch', event => {
-  try {
-    event.respondWith(handleEvent(event))
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
+    try {
+        event.respondWith(handleEvent(event));
+    } catch (error) {
+        event.respondWith(new Response(`An unexpected problem occurred: ${error.stack}`, { status: 500 }));
     }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
-  }
-})
+});
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
+    try {
+        // Get the request URL
+        const url = new URL(event.request.url);
+        
+        
+        // Get asset from KV (returns a Response object)
+        let response = await getAssetFromKV(event, {
+            mapRequestToAsset: serveSinglePageApp,
+            cacheControl: {
+                browserTTL: url.pathname.includes('/static/') ? 60 * 60 * 24 * 365 : url.pathname.includes('/images/') ? 60 * 60 * 24 : null,
+                edgeTTL: url.pathname.includes('/images/') ? 60 * 60 * 24 : 60 * 60 * 24 * 365,
+            },
+        });
 
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
 
-  try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      }
+        // Modify headers
+        response.headers.set('X-Content-Type-Options', 'nosniff');
+        response.headers.set('X-XSS-Protection', '1; mode=block');
+        response.headers.set('X-Frame-Options', 'SAMEORIGIN');
+        response.headers.set('Referrer-Policy', 'same-origin');
+
+
+        // Return response
+        return response;
+    } catch (error) {
+        try {
+            let pathname = new URL(event.request.url).pathname;
+            
+            if (error.message.includes('could not find')) {
+                return new Response(`Not found: ${pathname}`, { status: 404 });
+            }
+
+            return new Response(`An unexpected error occurred: ${error.stack}`, { status: 500 });
+        } catch (error) {
+            return new Response(`Error: ${error}`);
+        }
     }
-    return await getAssetFromKV(event, options)
-  } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
-    }
-
-    return new Response(e.message || e.toString(), { status: 500 })
-  }
-}
-
-/**
- * Here's one example of how to modify a request to
- * remove a specific prefix, in this case `/docs` from
- * the url. This can be useful if you are deploying to a
- * route on a zone, or if you only want your static content
- * to exist at a specific path.
- */
-function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
-
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
-
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
-  }
 }
