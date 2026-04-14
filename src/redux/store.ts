65833c6d8abb6f68b5e11ef575967234255ef879
Wes Cossick <wes@sparksuite.com>
2026-04-14 15:00:29 -0500

    Preserve state during HMR

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,8 +10,20 @@ if (process.env.NODE_ENV === 'development') {
 	middlewares.push(logger);
 }
 
-// Create store
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// Create store, restoring any state preserved across HMR update
+const store = createStore(
+	rootReducer,
+	module.hot?.data?.state,
+	applyMiddleware(...middlewares)
+);
+
+// Before this module is disposed during HMR update, save the current state
+//  -> That way, it can be restored when the module is re-evaluated above
+if (module.hot) {
+	module.hot.addDisposeHandler((data) => {
+		data.state = store.getState();
+	});
+}
 
 // Export
 export default store;
